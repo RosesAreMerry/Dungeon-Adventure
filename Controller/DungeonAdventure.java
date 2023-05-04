f73aab9e7c39fa631c76f8606802023d75684604
Chelsea Dacones <chelseadacones@gmail.com>
2023-05-04 14:09:30 -0700

    created outline

---
+++ Controller/DungeonAdventure.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import Model.Dungeon;
+import Model.Hero;
+import View.AdventureView;
+
+public class DungeonAdventure {
+    private Dungeon myDungeon;
+    private Hero myHero;
+    private AdventureView myAdventureView;
+
+    public void winGame() {
+
+    }
+    public void loseGame() {
+
+    }
+}
