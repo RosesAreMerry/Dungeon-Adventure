ae8af4a337d71040d14f6877cda05e2353bc4144
Maliha <hossainmaliha18@gmail.com>
2023-05-04 22:48:43 -0700

    outlined child classes of hero and monster and the view classes

---
+++ view/CombatView.java
@@ -0,0 +1,14 @@
+package view;
+
+public class CombatView {
+
+
+   public void  useAction (String s){
+
+   }
+
+   public void printChoices(String[] choice){
+
+   }
+
+}
---
+++ view/ConsoleView.java
@@ -0,0 +1,25 @@
+package view;
+
+public class ConsoleView {
+
+
+    private  void useItem(String use){
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+
+    public String sendMessage(String message){
+
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+    public String PropmtUSerChoice(String[] s) {
+
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+    public String show(){
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+    public String hide(){
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+
+}
