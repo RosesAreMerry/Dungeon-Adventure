a2af8511b5cd26492c146c85e916599eae4571c2
Maliha <hossainmaliha18@gmail.com>
2023-05-06 11:46:13 -0700

    commmented to code

--- model/DungeonCharacter.java
+++ model/DungeonCharacter.java
@@ -7,17 +7,27 @@ public class DungeonCharacter {
     private int myDamageMax;
 
     private int myAttackSpeed;
-    Double myHitChance;
+    private Double myHitChance;
 
+    /**
+     * initialize
+     */
     protected DungeonCharacter(){
         throw new UnsupportedOperationException("Method not yet implemented");
     }
 
+    /**
+     * returns true or false if the hitpoint is less than or equal to 0
+     * @return
+     */
     private boolean isFainted() {
         throw new UnsupportedOperationException("Method not yet implemented");
     }
 
     public String getName() {
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+    public String setName() {
         throw new UnsupportedOperationException("Method not yet implemented");
     }
 
@@ -32,6 +42,12 @@ public class DungeonCharacter {
     public int getAttackSpeed() {
         throw new UnsupportedOperationException("Method not yet implemented");
     }
+
+    /**
+     *
+     * @param DungeonCharacter
+     * @return
+     */
     public int  attack (String DungeonCharacter) {
         throw new UnsupportedOperationException("Method not yet implemented");
     }
