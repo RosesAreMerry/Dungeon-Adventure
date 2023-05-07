2fca9ff523e59f62763d0528ad1fd31e558b5c33
Chelsea Dacones <chelseadacones@gmail.com>
2023-05-07 13:39:57 -0700

    created inventory for Item's

--- model/Hero.java
+++ model/Hero.java
@@ -1,16 +1,43 @@
 package model;
 
-public class Hero extends DungeonCharacter{
-    private Double myBlockChance;
-    //private Item[] myInventory;
-    protected Hero() {
-        super();
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Hero extends DungeonCharacter{
+    private final int myMaxHealth;
+    private final double myBlockChance;
+    private final ArrayList<Item> myInventory;
+    private int myHealth;
+    public Hero(String theName, int theHitPoints, double theHitChance, int theDamageMin,
+                int theDamageMax, int theAttackSpeed, double theBlockChance) {
+        super(theName, theHitPoints, theHitChance, theDamageMin, theDamageMax, theAttackSpeed);
+        this.myBlockChance = theBlockChance;
+        int minHealth = 75;
+        int maxHealth = 100;
+        myHealth = getHitPoints();
+        myMaxHealth = new Random().nextInt(maxHealth + minHealth + 1) + minHealth;
+        myInventory = new ArrayList<>();
     }
     private int regularAttack(DungeonCharacter d){
         throw new UnsupportedOperationException("Method not yet implemented");
     }
     private int specialSkill(DungeonCharacter d){
         throw new UnsupportedOperationException("Method not yet implemented");
+    }
+    public void addToInventory(Item theItem) {
+        myInventory.add(theItem);
+    }
+    public ArrayList<Item> getInventory() {
+        return myInventory;
+    }
+    public void useHealingPotion(int theHealthRestore) {
+        myHealth += theHealthRestore;
+        if (myHealth > myMaxHealth) {
+            myHealth = myMaxHealth;
+        }
+    }
+    @Override
+    public void attack(DungeonCharacter theOpponent) {
+
     }
 }
