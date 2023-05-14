b39fbeac528df3d8383e9ff21cf6dceb81c73724
Rosemary Roach <rosemary@dopeapps.com>
2023-05-14 12:01:53 -0700

    Fixed merge conflics and a few errors

--- model/Monster.java
+++ model/Monster.java
@@ -83,4 +83,8 @@ public class Monster extends DungeonCharacter {
                 "\nMaximum Heal Points: " + getMyMaxHeal();
 
     }
+
+    public void setRandom(final Random theRandom) {
+        //TODO: Implement this
+    }
 }
